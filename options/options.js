38075711c8ada5cdd265376133bc249796bf8247
Crowdbotics <team@crowdbotics.com>
2024-04-29 15:17:44 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jorge-prompt-mvp-ve-47736",
+  url: "https://jorge-prompt-mvp-ve-47736.botics.co",
+  api: "https://jorge-prompt-mvp-ve-47736.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
